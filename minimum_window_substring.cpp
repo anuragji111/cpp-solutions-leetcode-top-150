fe1ef522e5c297c5733bd34efbc12969d55d995e
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-01-06 18:49:08 +0530

    Create minimum_window_substring.cpp

---
+++ minimum_window_substring.cpp
@@ -0,0 +1,53 @@
+Statement 33) Given two strings s and t of lengths m and n respectively, return the minimum window 
+substring
+ of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+
+--------------------------------------
+SOLUTION= class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for (char c : t) {
+            map[c]++;
+        }
+
+        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
+        while (end < s.size()){
+            if (map[s[end++]]-- > 0) {
+                counter--;
+            }
+            while (counter == 0) {
+                if (end - begin < d) {
+                    head = begin;
+                    d = end - head;
+                }
+                if (map[s[begin++]]++ == 0) {
+                    counter++;
+                }
+            }  
+        }
+        return d == INT_MAX ? "" : s.substr(head, d);
+    }
+};
+------------------------------------------------------
