85b7ac260503eca68b7d2642f7d1d85fb5d4d2d1
Anurag Kumar <140825596+Anuragji1@users.noreply.github.com>
2024-01-07 18:54:28 +0530

    Create two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,42 @@
+Statement 44) Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+-------------------------------------
+SOLUTION = class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i}; // Found the pair
+            }
+            seen[nums[i]] = i; // Store the current number and its index
+        }
+
+        // Should never reach here if a solution is guaranteed
+        return {};
+    }
+};
+
+--------------------------------------
